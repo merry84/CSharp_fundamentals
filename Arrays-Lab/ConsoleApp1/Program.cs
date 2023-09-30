c4822f7c46ec6bbfaba49c90be41fb381902135a
merry84 <maria_v_stamova@abv.bg>
2023-09-30 20:41:41 +0300

    update

---
+++ Arrays-Lab/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Read n numbers and print them in reverse order, separated by a single space.
+
+//First, we need to read n from the console.
+//Create an array of integers with n size.
+
+int n = int.Parse(Console.ReadLine());
+int[] numbers = new int[n];
+//Read n numbers using for loop.
+//Set number to the corresponding index.
+for (int i = 0; i < n; i++)
+{
+    int number = int.Parse(Console.ReadLine());
+    numbers[i] = number;
+}
+//Print the array in reversed order.
+for (int i = numbers.Length - 1; i >= 0 ; i--)
+{
+    Console.WriteLine(numbers[i] +" ");
+}
--- Arrays-Lab/DayOfWeek/Program.cs
+++ Arrays-Lab/DayOfWeek/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿//Enter a number in range 1-7 and print out the word representing it or "Invalid Day!".
+//Use an array of strings.
+
+string[] days = { "Monday", "Tuesday", "Wednesday",
+"Thursday", "Friday", "Saturday", "Sunday" };
+int day = int.Parse(Console.ReadLine());
+if (day >= 1 && day <= 7)
+    Console.WriteLine(days[day - 1]);
+else
+    Console.WriteLine("Invalid day!");
