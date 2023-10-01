ceed6690ff2a939e569a40774601cac4928ab0c5
merry84 <maria_v_stamova@abv.bg>
2023-10-01 11:32:06 +0300

    up

--- Arrays-Exercise/Train/Program.cs
+++ Arrays-Exercise/Train/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// A train has n number of wagons (integer, received as input).
+// On the next n lines, you will receive the number of people that are going to get on each wagon.
+//Print out the number of passengers in each wagon followed by the
+//total number of passengers on the train
