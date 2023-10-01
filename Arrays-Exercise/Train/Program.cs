54e542c2e2091705fd4d49a68df503bba64b2ac0
merry84 <maria_v_stamova@abv.bg>
2023-10-01 14:54:32 +0300

    update

--- Arrays-Exercise/Train/Program.cs
+++ Arrays-Exercise/Train/Program.cs
@@ -2,3 +2,14 @@
 // On the next n lines, you will receive the number of people that are going to get on each wagon.
 //Print out the number of passengers in each wagon followed by the
 //total number of passengers on the train
+int n = int.Parse(Console.ReadLine());
+int sum = 0;
+int[] wagons = new int[n];
+for (int i = 0; i < n; i++)
+{
+    int people = int.Parse(Console.ReadLine());
+    wagons[i] = people;
+    sum += wagons[i];
+}
+Console.WriteLine(string.Join(" ", wagons));
+Console.WriteLine(sum);
