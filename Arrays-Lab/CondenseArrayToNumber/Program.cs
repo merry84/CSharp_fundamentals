81ef9edd17596941fb1b7024f7e9e02de5ae3689
merry84 <maria_v_stamova@abv.bg>
2023-10-01 06:40:15 +0300

    sumarry

--- Arrays-Lab/CondenseArrayToNumber/Program.cs
+++ Arrays-Lab/CondenseArrayToNumber/Program.cs
@@ -10,5 +10,19 @@
 //•	Sum the numbers from nums[] to condensed[].
 //o	condensed[i] = nums[i] + nums[i + 1]
 //•	nums[] = condensed[]
-//The process is illustrated below:
 
+int[] numbers = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+while (numbers.Length>1)//Create a program to read an array of integers and condense them by summing
+// all adjacent couples of elements until a single integer remains.
+{
+    int[] condensed = new int[numbers.Length - 1];
+    for (int i = 0; i < numbers.Length - 1; i++)//Allocate a new array condensed[] of size nums.length.
+    {
+        condensed[i] = numbers[i] + numbers[i + 1];
+    }
+    numbers = condensed;
+}
+Console.WriteLine(numbers[0]);
