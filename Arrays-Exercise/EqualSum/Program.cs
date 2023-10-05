5c3a20d485b1c35ed8dc5a85b495a1bbbdb13493
merry84 <maria_v_stamova@abv.bg>
2023-10-05 19:30:35 +0300

    update

---
+++ Arrays-Exercise/EqualSum/Program.cs
@@ -0,0 +1,43 @@
+﻿// Create a program that determines if an element exists
+// in an array for which the sum of all elements to its left is 
+//equal to the sum of all elements to its right.
+//If there are no elements to the left or right, their sum is considered to 
+//be 0. Print the index of the element that satisfies the condition
+//or "no" if there is no such element
+
+int[] number = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+for (int i = 0; i <= number.Length - 1; i++)
+{
+    if (number.Length == 1)
+    {
+        Console.WriteLine(0);
+        return;
+    }
+    int sumLeft = 0;
+    for (int iLeft = i; iLeft > 0; iLeft--)
+    {
+        int nextPosition = iLeft - 1;
+        if (iLeft > 0)
+        {
+            sumLeft += number[nextPosition];
+        }
+    }
+    int sumRight = 0;
+    for (int iRight = i; iRight < number.Length; iRight++)
+    {
+        int nextPosition = (iRight + 1);
+        if (iRight < number.Length - 1)
+        {
+            sumRight += number[nextPosition];
+        }
+    }
+    if (sumLeft == sumRight)
+    {
+        Console.WriteLine(i);
+        return;
+    }
+}
+Console.WriteLine("no");
---
+++ Arrays-Exercise/TopIntegers/Program.cs
@@ -0,0 +1,26 @@
+﻿//Create a program to find all the top integers in an array.
+//A top integer is an integer that is greater than all the 
+//elements to its right
+int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+for (int i = 0; i < input.Length; i++)
+{
+    bool topInt = true;
+    for (int j = i + 1; j < input.Length; j++)
+    {
+        if (input[i] <= input[j])
+        {
+            topInt = false;
+        }
+
+
+
+    }
+    if (topInt)
+    {
+        Console.Write(input[i] + " ");
+
+    }
+
+
+}
+
