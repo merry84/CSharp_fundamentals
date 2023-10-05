c2e2f1008dd9b159da3a3cdf9e73839511ea3820
merry84 <maria_v_stamova@abv.bg>
2023-10-05 19:58:28 +0300

    update

--- Arrays-Exercise/MaxSequenceOfEqualElements/Program.cs
+++ Arrays-Exercise/MaxSequenceOfEqualElements/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Create a program that finds the longest sequence of equal elements in an array of integers.
+// If several equal sequences are present in the array, print out the leftmost one.
+int[] numbers = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+int count = 1;
+int countLenght = 0;
+int element = 0;
+for (int i = 0; i < numbers.Length - 1; i++)
+{
+    if (numbers[i] == numbers[i + 1])
+    {
+        count++;
+    }
+    else
+    {
+        count = 1;
+    }
+    if (countLenght < count)
+    {
+        countLenght = count;
+        element = numbers[i];
+    }
+}
+for (int j = 1; j <= countLenght; j++)
+{
+    Console.WriteLine($"{element}");
+
+}
