81ed28310e898c497a3efd5e3ce7d168bb745f2c
merry84 <maria_v_stamova@abv.bg>
2023-10-08 17:51:37 +0300

    update

---
+++ ExerciseMethods/AddAndSubtract/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace AddAndSubtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //You will receive 3 integers.
+            //Create a method that returns the sum of the first two integers
+            //and another method that subtracts the third integer from the result of the sum method.
+
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+
+            //SubstactsThirdResult(firstNum, secondNum, thirdNum);
+            int result = SubstractsThirdResult(firstNum, secondNum, thirdNum);  
+            Console.WriteLine(result);
+           
+        }
+        static int SubstractsThirdResult(int firstNum,int secondNum , int thirdNum)
+        {
+            int sum = SumFirstAndSecondNum(firstNum, secondNum);
+            return sum - thirdNum;
+        }
+        static int SumFirstAndSecondNum(int firstNum, int secondNum)
+        {
+            return firstNum + secondNum;
+        }
+    }
+}
---
+++ ExerciseMethods/PasswordValidator/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System;
+
+namespace PasswordValidator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a program that checks if a given password is valid.
+            // The password validation rules are:
+
+            //•	It should contain 6 – 10 characters(inclusive)
+            //•	It should contain only letters and digits
+            //•	It should contain at least 2 digits
+
+            //If it is not valid, for any unfulfilled rule print the corresponding message:
+            //•	"Password must be between 6 and 10 characters"
+            //•	"Password must consist only of letters and digits"
+            //•	"Password must have at least 2 digits"
+            //Hints
+           // Write a method for each rule.
+
+            var input = Console.ReadLine();
+            bool isValid = true;
+
+            if (!CharactersAll(input))
+            {
+                isValid = false;
+                Console.WriteLine("Password must be between 6 and 10 characters");
+            }
+            if (!LettersAndDigits(input))
+            {
+                isValid = false;
+                Console.WriteLine("Password must consist only of letters and digits");
+            }
+            if (!CharactersTwo(input))
+            {
+                isValid = false;
+                Console.WriteLine("Password must have at least 2 digits");
+
+            }
+            if (isValid == true)
+            {
+
+                Console.WriteLine("Password is valid");
+            }
+            static bool CharactersAll(string input)
+            {
+                return input.Length is >= 6 and <= 10;
+            }
+            static bool LettersAndDigits(string input)
+            {
+                return input.All(char.IsLetterOrDigit);
+            }
+            static bool CharactersTwo(string input)
+            {
+                return input.Count(char.IsDigit) >=2;
+
+            }
+        }
+    }
+}
