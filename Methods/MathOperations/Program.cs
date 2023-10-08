ad3158b5527f7aefaaf0caa4c0e465daa431fa7a
merry84 <maria_v_stamova@abv.bg>
2023-10-08 15:02:30 +0300

    update

---
+++ Methods/MathOperations/Program.cs
@@ -0,0 +1,44 @@
+﻿//Write a method that receives two numbers and an operator,
+//calculates the result and returns it.
+//You will be given three lines of input.
+//The first will be the first number,
+//the second one will be the operator
+//and the last one will be the second number.
+//The possible operators are: /, *, + and -.
+
+internal class Program
+{
+    private static void Main()
+    {
+        int firsNum = int.Parse(Console.ReadLine());
+        string operators = Console.ReadLine();
+        int secondNum = int.Parse(Console.ReadLine());
+
+        int resultSum = ResultOperations(firsNum, operators, secondNum);
+        Console.WriteLine(resultSum);
+        static int ResultOperations(int firsNum, string operators, int secondNum)
+        {
+
+            int result = 0;
+
+            if (operators == "+")
+            {
+                result = firsNum + secondNum;
+            }
+            else if (operators == "-")
+            {
+                result = firsNum - secondNum;
+            }
+            else if (operators == "*")
+            {
+                result = firsNum * secondNum;
+            }
+            else if (operators == "/")
+            {
+                result = firsNum / secondNum;
+            }
+            return result;
+
+        }
+    }
+} 
---
+++ Methods/MultiplyEvensByOdds1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Runtime.Intrinsics.X86;
+//Create a program that multiplies the sum of all even digits of a number
+//by the sum of all odd digits of the same number:
+//•	Create a method called GetMultipleOfEvenAndOdds()
+//•	Create a method GetSumOfEvenDigits()
+//•	Create GetSumOfOddDigits()
+//•	You may need to use Math.Abs() for negative numbers
+
+int number = Math.Abs(int.Parse(Console.ReadLine()));
+
+Console.WriteLine(GetMultipleOfEvenAndOdds(number));
+
+static int GetMultipleOfEvenAndOdds(int a)
+{
+    int result = GetSumOfEvenDigits(a) * GetSumOfOddDigits(a);
+    
+    return result;
+}
+
+static int GetSumOfEvenDigits(int number)
+{
+    int sumEven = 0;
+    while (number > 0)
+    {
+        int lastDigit = number % 10;
+        if (lastDigit % 2 == 0)// четно
+        {
+            sumEven += lastDigit;
+        }
+        number /= 10;
+    }
+    return sumEven;
+}
+
+
+static int GetSumOfOddDigits(int number)
+{
+    int sumOdd = 0;
+    while (number > 0)
+    {
+        int lastDigit = number % 10;
+        if (lastDigit % 2 == 1) //нечетно
+        {
+            sumOdd += lastDigit;
+        }
+        number /= 10;
+
+    }
+    return sumOdd;
+
+}
