7127cf211b67d87cb02fbb97c6b533eafc59e69f
merry84 <maria_v_stamova@abv.bg>
2023-10-18 08:51:32 +0300

    update

---
+++ Lists-More Exercise/CarRace/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace CarRace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Write a program to calculate the winner of a car race.
+            // You will receive an array of numbers.
+            // Each element of the array represents the time needed to pass through that step (the index).
+            // There are going to be two cars.
+            // One of them starts from the left side and the other one starts from the right side.
+            // The middle index of the array is the finish line. The number of elements in the array will always be odd.
+            // Calculate the total time for each racer to reach the finish,
+            // which is the middle of the array, and print the winner with his total time (the racer with less time).
+            // If you have a zero in the array, you have to reduce the time of the racer that reached
+            // it by 20% (from his current time).
+
+            //Print the result in the following format "The winner is {left/right} with total time: {total time}".
+
+            List<double> carRace = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToList();
+            double sumFirstCar = SumFirstCar(carRace);
+            double sumSecondCar = SumSecondCar(carRace);
+            if (sumFirstCar > sumSecondCar)
+            {
+                Console.WriteLine($"The winner is right with total time: {Math.Floor(sumSecondCar)}");
+            }
+            else
+            {
+                Console.WriteLine($"The winner is left with total time: {sumFirstCar:f1}");
+            }
+            
+        }
+        static double SumFirstCar(List<double> carRace)
+        {
+            double sum = 0;
+            for (int i = 0; i < carRace.Count / 2; i++)
+            {
+                double currentNumber = carRace[i];
+                if (currentNumber == 0)
+                {
+                    sum = sum * 0.8;
+                }
+                sum += currentNumber;
+            }
+            return sum;
+        }
+        static double SumSecondCar(List<double> carRace)
+        {
+            double sum = 0;
+            for (int j = carRace.Count - 1; j > carRace.Count / 2; j--)
+            {
+                double currentNumber = carRace[j];
+                if (currentNumber == 0)
+                {
+                    sum = sum * 0.8;
+                }
+                sum += currentNumber;
+            }
+            return sum;
+        }
+    }
+}
---
+++ Lists-More Exercise/Messaging/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace Messaging
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //You will be given a list of numbers and a string.
+            //For each element of the list you must calculate the sum of its digits and take the element,
+            //corresponding to that index from the text.If the index is greater than the length of the text,
+            //start counting from the beginning (so that you always have a valid index
+            //After you get that element from the text,
+            //you must remove the character you have taken
+            //from it(so for the next index the text will be with one characterless)
+
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            List<char> text = Console.ReadLine()
+                .ToList();
+            List<char> letter = new List<char>();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                int num = IndexFromText(numbers[i], text.Count);
+                letter.Add(text[num]);
+                text.RemoveAt(num);
+            }
+            Console.WriteLine(string.Join("", letter));
+
+            static int SumOfDigits(int number)
+            {
+                int result = 0;
+                while (number > 0)
+                {
+                    int lastDigit = number % 10;
+                    result += lastDigit;
+                    number /= 10;
+                }
+                return result;
+            }
+            static int IndexFromText(int number, int count)
+            {
+                int index = SumOfDigits(number);
+                if (index > count)
+                {
+                    if (index % count == 0)
+                    {
+                        index = count - 1;
+                    }
+                    else
+                    {
+                        index %= count;
+                    }
+                }
+                return index;
+            }
+        }
+    }
+}
