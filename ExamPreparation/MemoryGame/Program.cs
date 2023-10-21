0e509e6d16f690c3f2fd9583ba99293a658a35ac
merry84 <maria_v_stamova@abv.bg>
2023-10-21 17:49:51 +0300

    update

---
+++ ExamPreparation/MemoryGame/Program.cs
@@ -0,0 +1,118 @@
+﻿namespace MemoryGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            {
+                // Write a program that recreates the Memory game.
+                //On the first line, you will receive a sequence of elements. Each element in the sequence will have a twin.
+                //Until the player receives "end" from the console, you will receive strings with two integers separated by a space,
+                //representing the indexes of elements in the sequence.
+
+                //If the player tries to cheat and enters two equal indexes or indexes which are out of bounds of the sequence,
+                //you should add two matching elements at the middle of the sequence in the following format:
+                //            "-{number of moves until now}a"
+                //Then print this message on the console:
+                //"Invalid input! Adding additional elements to the board"
+
+                //Input
+                //· On the first line, you will receive a sequence of elements
+                //· On the following lines, you will receive integers until the command "end"
+                //Output
+                //· Every time the player hit two matching elements, you should remove them from the sequence and print on the console the following message:
+                //"Congrats! You have found matching elements - ${element}!"
+                //· If the player hit two different elements, you should print on the console the following message:
+                //            "Try again!"
+                //· If the player hit all matching elements before he receives "end" from the console, you should print on the console the following message:
+                //            "You have won in {number of moves until now} turns!"
+                //· If the player receives "end" before he hits all matching elements, you should print on the console the following message:
+                //"Sorry you lose :(
+                //{the current sequence's state}"
+                //Constraints
+                //· All elements in the sequence will always have a matching element.
+
+                List<string> sequence = Console.ReadLine()
+                    .Split()
+                    .ToList();
+                string input = string.Empty;
+                int moveCount = 0;
+
+                while ((input = Console.ReadLine()) != "end")
+                {
+                    List<int> elements = input
+                        .Split()
+                        .Select(int.Parse)
+                        .ToList();
+                    int firstElement = elements[0];
+                    int secondElement = elements[1];
+                    moveCount++;
+                    if ((firstElement == secondElement) //If the player tries to cheat and enters two equal indexes or indexes which are out of bounds of the sequence
+                        || (firstElement < 0 || firstElement >= sequence.Count)
+                        || (secondElement < 0 || secondElement >= sequence.Count))
+                    {
+                        string newSymbol = $"-{moveCount}a";
+                        //you should add two matching elements at the middle of the sequence 
+                        List<string> newElements = new List<string> { newSymbol, newSymbol };
+                        int middleIndex = sequence.Count / 2;//middle of the sequence 
+                        sequence.InsertRange(middleIndex, newElements);
+                        Console.WriteLine("Invalid input! Adding additional elements to the board");
+
+                    }
+                    else
+                    {
+                        //Every time the player hit two matching elements, you should remove them from the sequence and print on the console the following message:
+                        //"Congrats! You have found matching elements - ${element}!"
+                        if (sequence[firstElement] == sequence[secondElement])
+                        {
+                            string element = sequence[firstElement];
+                            Console.WriteLine($"Congrats! You have found matching elements - {element}!");
+                            if (firstElement < secondElement)//you should remove them from the sequence
+                            {
+                                sequence.RemoveAt(firstElement);
+                                sequence.RemoveAt(secondElement - 1);
+                            }
+                            else if (firstElement > secondElement)
+                            {
+                                sequence.RemoveAt(secondElement);
+                                sequence.RemoveAt(firstElement - 1);
+                            }
+                            // If the player hit two different elements, you should print on the console the following message:
+                            //            "Try again!"
+
+                        }
+                        else if (sequence[firstElement] != sequence[secondElement])
+                        {
+                            Console.WriteLine("Try again!");
+                        }
+
+
+                        //If the player hit all matching elements before he receives "end" from the console, you should print on the console the following message:
+                        //            "You have won in {number of moves until now} turns!"
+                        if (sequence.Count == 0)
+                        {
+                            Console.WriteLine($"You have won in {moveCount} turns!");
+                            break;
+                        }
+
+
+                    }
+
+                    //If the player receives "end" before he hits all matching elements, you should print on the console the following message:
+                    //"Sorry you lose :(
+                    //{the current sequence's state}"
+
+                }
+                if (sequence.Count != 0)
+                {
+                    Console.WriteLine("Sorry you lose :(");
+                    Console.WriteLine(string.Join(" ", sequence));
+                }
+            }
+
+
+        }
+    }
+}
+    
---
+++ ExamPreparation/ShootForTheWin/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace ShootForTheWin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that helps you keep track of your shot targets.
+            // You will receive a sequence with integers, separated by a single space, representing targets and their value.
+            // Afterward, you will be receiving indices until the "End" command is given,
+            // and you need to print the targets and the count of shot targets.
+
+            //Every time you receive an index, you need to shoot the target on that index, if it is possible.
+            //Every time you shoot a target, its value becomes -1, and it is considered shot. Along with that, you also need to:
+
+            //•	Reduce all the other targets, which have greater values than your current target, with its value. 
+
+            //•	Increase all the other targets, which have less than or equal value to the shot target, with its value.
+
+            //Keep in mind that you can't shoot a target, which is already shot. You also can't increase or reduce a target,
+            //which is considered a shot.
+            //When you receive the "End" command, print the targets in their current state and the count of shot targets in the following format:
+            //   "Shot targets: {count} -> {target1} {target2}… {targetn}"
+            //Input / Constraints
+            //•	On the first line of input, you will receive a sequence of integers, separated by a single space – the targets sequence.
+            //•	On the following lines, until the "End" command, you be receiving integers each on a single line – the index of the target to be shot.
+            //Output
+            //•	The format of the output is described above in the problem description.
+
+            List<int> targetSequence = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            string input;
+            int shoot = 0;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                int targetIndex = int.Parse(input);
+                if (targetIndex < 0 || targetIndex >= targetSequence.Count)//if it is possible.
+                {
+                    continue;
+                }
+
+                int targetValue = targetSequence[targetIndex];//Every time you shoot a target, its value becomes -1, and it is considered shot.
+
+                if (targetValue == -1)
+                {
+                    continue;
+                }
+
+                targetSequence[targetIndex] = -1;//shoot a target, its value becomes -1!!!
+                shoot++;
+
+                for (int i = 0; i < targetSequence.Count; i++)
+                {
+                    if (targetSequence[i] == -1)
+                    {
+                        continue;
+                    }
+                    else if (targetValue >= targetSequence[i])
+                    {
+                        targetSequence[i] += targetValue;//Increase all the other targets, which have less than or equal value to the shot target, with its value.
+                    }
+                    else if (targetValue < targetSequence[i])
+                    {
+                        targetSequence[i] -= targetValue;//Reduce all the other targets, which have greater values than your current target, with its value.
+                    }
+                }
+
+            }
+            Console.WriteLine($"Shot targets: {shoot} -> {string.Join(" ", targetSequence)}");
+        }
+    }
+}
